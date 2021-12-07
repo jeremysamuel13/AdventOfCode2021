1269bb158b4cdb7bfa2363c5e582a3cba5346de2
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2021-12-07 04:26:47 -0500

    day7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,3 @@
-use std::iter::repeat;
-
 const FILEPATH: &str = "src/input.txt";
 type DataType = i32;
 
@@ -7,30 +5,29 @@ fn main() {
     let data: Vec<DataType> = std::fs::read_to_string(FILEPATH)
         .expect("Unable to read file")
         .trim()
-        .split(",")
+        .split(',')
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
     println!("Q1 answer is: {}", q1(&data));
     println!("Q2 answer is: {}", q2(&data));
+
+    assert_eq!(q1(&data), 336120);
+    assert_eq!(q2(&data), 96864235);
 }
 
-fn q1(data: &Vec<DataType>) -> i32 {
-    vec![0; *data.iter().max().unwrap() as usize + 1]
-        .iter()
-        .enumerate()
-        .map(|(i, val)| data.iter().map(|x| (x - i as i32).abs()).sum::<i32>())
+fn q1(data: &[DataType]) -> i32 {
+    (*data.iter().min().unwrap()..=*data.iter().max().unwrap())
+        .map(|i| data.iter().map(|x| (x - i).abs()).sum::<i32>())
         .min()
         .unwrap()
 }
 
-fn q2(data: &Vec<DataType>) -> i32 {
-    vec![0; *data.iter().max().unwrap() as usize + 1]
-        .iter()
-        .enumerate()
-        .map(|(i, _)| {
+fn q2(data: &[DataType]) -> i32 {
+    (*data.iter().min().unwrap()..=*data.iter().max().unwrap())
+        .map(|i| {
             data.iter()
-                .map(|x| (x - i as i32).abs() * ((x - i as i32).abs() + 1) / 2)
+                .map(|x| (x - i).abs() * ((x - i).abs() + 1) / 2)
                 .sum::<i32>()
         })
         .min()
